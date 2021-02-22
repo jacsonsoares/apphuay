5b21591c9b59584e6e3605a4a094484e3688e611
Jacson Soares <jacsonsoares@hotmail.com>
2021-02-22 00:28:39 -0300

    erro unform corrigido e button tambem acertado.

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components/native';
 import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled(RectButton)`
-  width: 100%;
+  min-width: 100%;
   height: 60px;
   background-color: #F4EDE8;
   border-radius : 10px;
